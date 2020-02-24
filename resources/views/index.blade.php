360b13abc7f12563c4802b9116a254c3f42501fd
Kolyakolya1 <58611145+Kolyakolya1@users.noreply.github.com>
2020-02-24 19:50:41 +0200

    shop1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('title','Главная ')
+@section('content')
+
+    <div class="starter-template">
+        <h1>Все товары</h1>
+        <div class="row">
+            @include('card')
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/product.blade.php
@@ -0,0 +1,18 @@
+@extends('master')
+@section('title','Товар ')
+@section('content')
+    <div class="starter-template">
+        <h1>HTC One S</h1>
+        <h2>{{$product}}</h2>
+        <h2>Мобильные телефоны</h2>
+        <p>Цена: <b>12490 руб.</b></p>
+        <img src="http://internet-shop.tmweb.ru/storage/products/htc_one_s.png">
+        <p>Зачем платить за лишнее? Легендарный HTC One S</p>
+
+        <form action="/basket/add/3" method="POST">
+            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
+
+            <input type="hidden" name="_token" value="txEX9GLQe2KPNyrY7w9G3dZ8ziR5Bui4yZENdW37">        </form>
+    </div>
+@endsection
+
